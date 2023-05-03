13155cf6717ea83d5b34b98c208fb848103540ad
rekil2g <kevinlinux2020@gmail.com>
2023-05-03 14:17:04 -0300

    Primeiro exercicio

--- Python/ex001.py
+++ Python/ex001.py
@@ -2,4 +2,10 @@
 # Exercícios resolvidos: 001; 002; 003.
 nome = input('Qual o seu nome?\n')
 print(f'Ola {nome}, bem-vindo ao terminal!')
-
+valor_um = int(input('Digite um valor para calcular: '))
+valor_dois = int(input('Digite mais um valor: '))
+soma = valor_um + valor_dois
+print(f''' O antecessor de {soma} e: {soma - 1},
+ A soma de {valor_um} + {valor_dois} e de {soma},
+ O sucessor de {soma} e: {soma + 1}.
+	''')
