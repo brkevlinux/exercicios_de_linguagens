26da41508b23f116a20f127e15108503c18c6b23
rekil2g <kevinlinux2020@gmail.com>
2023-05-06 07:47:44 -0300

    criando pasta de java

---
+++ Java/exercicio01/src/Ex001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ex001 {
+
+	public static void main(String[] args) {
+		// Exercicios em java.
+		Scanner tecla = new Scanner(System.in);
+		System.out.println("Olá qual é o seu nome?");
+		String nome = tecla.nextLine();
+		System.out.format("Seja bem vindo %s \n", nome);
+		System.out.print("Digite a sua primeira nota: ");
+		float nota1 = tecla.nextFloat();
+		System.out.print("Digite a sua segunda nota: ");
+		float nota2 = tecla.nextFloat();
+		float media = (nota1 + nota2) / 2f;
+		System.out.format("A sua média é: %.2f", media);
+
+	}
+
+}
