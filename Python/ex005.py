c18c30111f6148751d13fdf9413fa931fc4e447f
rekil2g <kevinlinux2020@gmail.com>
2023-08-19 14:04:52 -0300

    organizando valores

--- Python/ex005.py
+++ Python/ex005.py
@@ -3,14 +3,14 @@
 
 valor = int(input('Digite um numero inteiro: '))
 print(f'''
-	|{valor} X 0 = {valor * 0}|
-	|{valor} X 1 = {valor * 1}|
-	|{valor} X 2 = {valor * 2}|
-	|{valor} X 3 = {valor * 3}|
-	|{valor} X 4 = {valor * 4}|
-	|{valor} X 5 = {valor * 5}|
-	|{valor} X 6 = {valor * 6}|
-	|{valor} X 7 = {valor * 7}|
-	|{valor} X 8 = {valor * 8}|
-	|{valor} X 9 = {valor * 9}|
+	|{valor} X 0 = {valor * 0:2}|
+	|{valor} X 1 = {valor * 1:2}|
+	|{valor} X 2 = {valor * 2:2}|
+	|{valor} X 3 = {valor * 3:2}|
+	|{valor} X 4 = {valor * 4:2}|
+	|{valor} X 5 = {valor * 5:2}|
+	|{valor} X 6 = {valor * 6:2}|
+	|{valor} X 7 = {valor * 7:2}|
+	|{valor} X 8 = {valor * 8:2}|
+	|{valor} X 9 = {valor * 9:2}|
 	''')
