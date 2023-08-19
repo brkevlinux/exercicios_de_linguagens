10ec28250f51d20ac6d525f82b0e2dd46ebf4414
rekil2g <kevinlinux2020@gmail.com>
2023-08-19 11:29:31 -0300

    ediçao na nova branch

--- Python/ex003.py
+++ Python/ex003.py
@@ -1,1 +1,13 @@
-# Treino de Python
+# Exercício 03 de Python.
+#Exercícios 006
+
+numero = int(input('Digite um numero inteiro: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+print(f''' O numero digitado foi {numero};
+	O dobro do numero é: {dobro};
+	O triplo do numero e: {triplo};
+	A sua raiz quadrada é: {raiz}
+	''')
+print(f'A média aritimetica é: {(numero + dobro + triplo + raiz) / 4}')
