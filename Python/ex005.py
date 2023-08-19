047bfb18e6ce76dac9bf791ca1e20fedfcac9be7
rekil2g <kevinlinux2020@gmail.com>
2023-08-19 13:58:22 -0300

    adicionando exercicio 5

---
+++ Python/ex005.py
@@ -0,0 +1,16 @@
+# Exercicio de Python V.3.
+# Tabuada de um numero inteiro.
+
+valor = int(input('Digite um numero inteiro: '))
+print(f'''
+	|{valor} X 0 = {valor * 0}|
+	|{valor} X 1 = {valor * 1}|
+	|{valor} X 2 = {valor * 2}|
+	|{valor} X 3 = {valor * 3}|
+	|{valor} X 4 = {valor * 4}|
+	|{valor} X 5 = {valor * 5}|
+	|{valor} X 6 = {valor * 6}|
+	|{valor} X 7 = {valor * 7}|
+	|{valor} X 8 = {valor * 8}|
+	|{valor} X 9 = {valor * 9}|
+	''')
