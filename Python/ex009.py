d819f01d08c0ecd02fd869bfeca5ac8e4b4440a7
rekil2g <kevinlinux2020@gmail.com>
2023-08-20 09:06:08 -0300

    exercicio numero 13

---
+++ Python/ex009.py
@@ -0,0 +1,10 @@
+# Exercicio de Python V.3.
+# Exercicio numero 13
+
+sal = float(input('Qual é o seu salario atual? R$ '))
+ajuste = int(input('Qual é a porcentagem de ajuste? '))
+print(f'''
+O seu salario atual de R${sal:.2f},
+com o aumento de {ajuste}% 
+ira valer R${sal + (sal * ajuste / 100):.2f}
+''')
