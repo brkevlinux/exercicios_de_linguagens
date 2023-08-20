ef1be3b43bb2dab9078ab42539edb58e80a2e244
rekil2g <kevinlinux2020@gmail.com>
2023-08-20 09:28:16 -0300

    exercicio numero 14

---
+++ Python/ex010.py
@@ -0,0 +1,8 @@
+# Exercicios de Python V.3.
+# Exercicio numero 14.
+
+c = float(input('Digite a temperatura em Celcius: '))
+print(f'''
+A temperatura de {c:.2f}ºC convertida para Fahrenheit
+tem o valor de {9 * c / 5 + 32:.2f}ºF!
+''')
