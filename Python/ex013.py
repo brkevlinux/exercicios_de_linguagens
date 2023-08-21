cc356d3624d458e262b6eec4bc705264a80f4a62
rekil2g <kevinlinux2020@gmail.com>
2023-08-21 08:04:35 -0300

    segunda parte do ex013

--- Python/ex013.py
+++ Python/ex013.py
@@ -1,7 +1,7 @@
 # Exercicio de Python V.3.
-# Execicio numero 17.
+# Execicio numero 17, 18.
 
-from math import hypot
+from math import hypot, sin, cos, tan, radians
 cat_a = float(input('Comprimento do cateto adjacente: '))
 cat_o = float(input('Comprimento do cateto oposto: '))
 hipotenusa = hypot(cat_o, cat_a)
@@ -10,3 +10,11 @@ print(f'''
 	O valor do cateto oposto e de {cat_o:.2f}
 	A hipotenusa do triangulo vale: {hipotenusa:.2f}
 ''')
+
+angulo = float(input('Digite um angulo: '))
+print(f'''
+	Para o angulo de {angulo:.2f}º temos:
+	O seno de {sin(radians(angulo)):.2f}
+	O cosseno de {cos(radians(angulo)):.2f}
+	E a tangente de {tan(radians(angulo)):.2f}
+	''')
