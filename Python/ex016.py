1df41cdd8c1dea24275ad4e9abb15b9bd1f35677
rekil2g <kevinlinux2020@gmail.com>
2023-08-22 07:55:43 -0300

    Adicionando es016

---
+++ Python/ex016.py
@@ -0,0 +1,11 @@
+# Exercicio de Python V.3.
+# Exercicio numero 22.
+
+nome = str(input('Digite o seu nome: '))
+primeiro = nome.split()
+print(f'''
+	Seu primeiro nome e {primeiro[0]} e possui {len(primeiro[0])}
+	Seu nome ao todo tem {len(nome.strip()) - nome.count(' ')} letras.
+	Seu nome em minusculas {nome.lower()}
+	Seu nome em maiusculas {nome.upper()}
+	''')
