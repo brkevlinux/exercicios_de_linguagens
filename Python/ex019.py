7f76ced9d481d97704a594be869236a166d24014
rekil2g <kevinlinux2020@gmail.com>
2023-08-22 11:01:43 -0300

    correção do ex019

--- Python/ex019.py
+++ Python/ex019.py
@@ -7,5 +7,5 @@ print(f'''
 	Analizando a frase temos:
 	A letra A aparece na posição: {verificar.find('a') + 1} primeiro.
 	A letra A aparece {verificar.count('a')} vezes.
-	A ultima posicao em que a letra A aparece  e: {verificar.rfind() + 1}
+	A ultima posicao em que a letra A aparece  e: {verificar.rfind('a') + 1}
 	''')
