11f466bf7501347abca5bc182d8d515c35e77f26
rekil2g <kevinlinux2020@gmail.com>
2023-08-22 09:38:42 -0300

    esta com erro no rfind

---
+++ Python/ex019.py
@@ -0,0 +1,11 @@
+# Exercicio de Python V.3.
+# Exercicio numero 26.
+
+frase = str(input('Digite uma frase: \n'))
+verificar = frase.strip().lower()
+print(f'''
+	Analizando a frase temos:
+	A letra A aparece na posição: {verificar.find('a') + 1} primeiro.
+	A letra A aparece {verificar.count('a')} vezes.
+	A ultima posicao em que a letra A aparece  e: {verificar.rfind() + 1}
+	''')
