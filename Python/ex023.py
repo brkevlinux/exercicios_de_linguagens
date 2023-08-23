db00aad26cb09c4c2801f6101e92266c8c2d3bac
rekil2g <kevinlinux2020@gmail.com>
2023-08-23 07:19:32 -0300

    adicionado o ex023

---
+++ Python/ex023.py
@@ -0,0 +1,8 @@
+# Exercicio de Python V.3.
+# Exercicio numero 30.
+
+num = int(input('Digite um numero inteiro: '))
+if (num % 2) == 0:
+	print(f'O numero {num} é par')
+else:
+	print(f'O numero {num} é impar')
