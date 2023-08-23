12ff179d89b561bc60070206e33a61e379ad97ac
rekil2g <kevinlinux2020@gmail.com>
2023-08-23 08:34:04 -0300

    Adicionado o ex027

---
+++ Python/ex027.py
@@ -0,0 +1,8 @@
+# Exercicio de Python V.3.
+# Exercicio numero 13
+
+sal = float(input('Qual é o seu salario atual? R$ '))
+if sal <= 1250:
+	print(f'O salario de {sal} passa a valer R${sal + (sal * (15 / 100)):.2f}')
+else:
+	print(f' O salario de {sal} passa a valer R${sal + (sal * (10 / 100)):.2f}')
